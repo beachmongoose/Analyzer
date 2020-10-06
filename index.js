0074375549f53ed5469828806cdedb9d9e2bc833
Maggie Maldjian <maggiemaldjian@gmail.com>
2020-10-06 18:00:55 -0400

    adding functionality

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Discord = require('discord.js');
-const { prefix, token, botID } = require('./config.json')
+const { prefix, token, botID,
+    stockQueries, stockAnswers,
+    stockThanks, stockCompliments,
+    stockThanksReturn,
+    archiveNames,
+    aboutQuery, aboutAnswer } = require('./config.json')
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -7,8 +12,7 @@ client.once('ready', () => {
 });
 
 client.on('message', (message) => {
-    console.log(message.content)
-     if (message.author.bot) {
+    if (message.author.bot) {
          return; }
     if (botMentioned(message) === true) {
         askAnalyzer(message)
@@ -20,11 +24,11 @@ client.on('message', (message) => {
 });
 
 function prefixCommand(message) {
-    const input = message.content.slice(prefix.length).toLowerCase()
+    const input = message.content.slice(prefix.length).trim().split(' ');
     const command = input.shift().toLowerCase();
     switch(command) {
         case 'file':
-            console.log('file')
+            accessFile(input, message)
             break;
         case 'calculate':
             calculate(input, message)
@@ -45,23 +49,76 @@ function prefixCommand(message) {
     }
     }
 
+function accessFile(input, message) {
+    let query = String(input[0]).toLowerCase()
+    console.log()
+    for (entry in archiveNames) {
+        let name = String(archiveNames[entry])
+        if (query === name) {
+            let number = 40;
+            let imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
+            message.channel.send ( { files: ["./images/"+ name + "/" + imageNumber + ".png"]})
+        }
+     }
+}
+
+
+
 function calculate(input, message) {
-    const request = input.replace('calculate','');
+    const request = String(input).replace('calculate','');
     console.log(request)
-    if (!request.length) {
+    if (request.length <= 1) {
         message.channel.send `NO QUERY PROVIDED.`
         return;
     }
-    message.channel.send(`CALCULATED PROBABILITY IS ` + Math.floor(Math.random() * 101) + `%`)
+    message.channel.send(`CALCULATED PROBABILITY IS ` + Math.floor(Math.random() * 101) + `%.`)
+    return;
 }
 
 function askAnalyzer(message) {
-    if (!message.content.startsWith(botID)) {
+    if (!message.content.startsWith(botID) && (!message.content.startsWith(`<@!539521677227327499>`))) {
         return message.channel.send `PRESENT.`
     }
-    const input = String(message.content.slice(botID.length).toLowerCase())
-    console.log(input)
+    let idCount = getIDCount(message)
+    const input = String(message.content.slice(idCount.length).toLowerCase())
+    if (input === "") {
+        message.channel.send `PRESENT.`
+        return
+      }
+    checkForPhrasesIn(input, message)
+}
 
+function checkForPhrasesIn(input, message) {
+    for (entry in stockQueries) {
+        let keyPhrase = String(stockQueries[entry])
+        if (input.includes(keyPhrase)) {
+            message.channel.send(stockAnswers[entry])
+            return;
+        }
+    }
+    for (entry in stockCompliments) {
+        let keyPhrase = String(stockCompliments[entry])
+        if (input.includes(keyPhrase)) {
+            let name = String(message.author).toUpperCase()
+            message.channel.send(`THANK YOU, ` + name + `.`)
+            return;
+        }
+    }
+    for (entry in stockThanks) {
+        let keyPhrase = String(stockThanks[entry])
+        if (input.includes(keyPhrase)) {
+            let name = String(message.author).toUpperCase()
+            message.channel.send(randomItem(stockThanksReturn))
+            return;
+        }
+    }
+    for (entry in aboutQuery) {
+        let keyPhrase = String(aboutQuery[entry])
+        if (input.includes(keyPhrase)) {
+            message.channel.send(aboutAnswer)
+            return;
+        }
+    }
     if (input.includes('calculate')) {
         calculate(input, message)
         return
@@ -75,6 +132,19 @@ function botMentioned(message) {
     } else {
         return false
     }
+}
+
+function getIDCount(message) {
+    let text = String(message.content)
+    if (text.includes `<@!539521677227327499>`) {
+        return `<@!539521677227327499> `
+    } else {
+        return `<@539521677227327499> `
+    }
+}
+
+function randomItem(items) {
+    return items[Math.floor(Math.random()*items.length)];
 }
 
 client.login(token)
